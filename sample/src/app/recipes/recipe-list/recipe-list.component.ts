82f45abcbcde5bf95a37306e03ce8bd401479ee5
binodgupta <binodgupta010>
2018-11-23 03:23:22 -1200

    first commit

---
+++ sample/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+ recipes :Recipe[] = [
+   new Recipe("The Test Recipe",'this is sample test',"https://img.taste.com.au/4-rDZU7z/w643-h428-cfill-q90/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg")
+   ,new Recipe("The Test Recipe",'this is sample test',"https://img.taste.com.au/4-rDZU7z/w643-h428-cfill-q90/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg")
+   ,new Recipe("The Test Recipe",'this is sample test',"https://img.taste.com.au/4-rDZU7z/w643-h428-cfill-q90/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg")
+   ,new Recipe("The Test Recipe",'this is sample test',"https://img.taste.com.au/4-rDZU7z/w643-h428-cfill-q90/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg")
+   ,new Recipe("The Test Recipe",'this is sample test',"https://img.taste.com.au/4-rDZU7z/w643-h428-cfill-q90/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg")
+   ,new Recipe("The Test Recipe",'this is sample test',"https://img.taste.com.au/4-rDZU7z/w643-h428-cfill-q90/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg")
+   ,new Recipe("The Test Recipe",'this is sample test',"https://img.taste.com.au/4-rDZU7z/w643-h428-cfill-q90/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg")
+   ,new Recipe("The Test Recipe",'this is sample test',"https://img.taste.com.au/4-rDZU7z/w643-h428-cfill-q90/taste/2016/11/chicken-and-prosciutto-parmigiana-79468-1.jpeg")
+ ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
